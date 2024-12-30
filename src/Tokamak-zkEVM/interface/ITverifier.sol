bbace06cad8e4f6479b03b6c297af8fd5172eabc
mehdi-defiesta <mberiane@defiesta.net>
2024-12-30 12:10:13 +0100

    recompute challenges + tests

--- src/Tokamak-zkEVM/interface/ITverifier.sol
+++ src/Tokamak-zkEVM/interface/ITverifier.sol
@@ -5,12 +5,10 @@ pragma solidity 0.8.20;
 /// @author TOKAMAK project Ooo
 interface ITverifier {
     /// @dev Verifies a zk-SNARK proof.
-    /// @return A boolean value indicating whether the zk-SNARK proof is valid.
     /// Note: The function may revert execution instead of returning false in some cases.
     function verify(
         uint256[] calldata _publicInputs,
-        uint256[] calldata _proof,
-        uint256[] calldata _recursiveAggregationInput
+        uint256[] calldata _proof
     ) external view returns (bool);
 
 }
